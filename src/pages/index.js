3be0a049fa2f93a9e4825dba1ac0043bb6049a7f
Olivier <olivierchemla@gmail.com>
2019-08-04 00:21:39 +0200

    Updates Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,97 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import Terminal from "../components/terminal"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <section className="mb-5">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>
+            <strong>Who</strong> we are ?
+          </h2>
+          <p className="mb-5 text-primary">
+            Nos valeurs, nos besoins, nos respects.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aut
+            iusto facere eaque aspernatur nam, alias debitis voluptatem
+            adipisci, repudiandae earum magni ab, vitae nobis natus ut deserunt
+            dicta itaque.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <Terminal />
+        </div>
+      </div>
+    </section>
+
+    <section className="mb-5">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: "25%}" }}
+              aria
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+        </div>
+        <div className="col-md-6 text-right">
+          <h2>
+            Our <strong>Stack</strong>
+          </h2>
+          <p className="mb-5 text-primary">
+            Les outils que nous utilisons pour votre projets :
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aut
+            iusto facere eaque aspernatur nam, alias debitis voluptatem
+            adipisci, repudiandae earum magni ab, vitae nobis natus ut deserunt
+            dicta itaque.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section className="mb-5">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: "25%}" }}
+              aria
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+        </div>
+        <div className="col-md-6 text-right">
+          <h2>
+            Our <strong>Stack</strong>
+          </h2>
+          <p className="mb-5 text-primary">
+            Les outils que nous utilisons pour votre projets :
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aut
+            iusto facere eaque aspernatur nam, alias debitis voluptatem
+            adipisci, repudiandae earum magni ab, vitae nobis natus ut deserunt
+            dicta itaque.
+          </p>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 
---
+++ src/pages/projects.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ProjectsPage = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <h1>Projects realize by our team :</h1>
+    <p>lor</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ProjectsPage
